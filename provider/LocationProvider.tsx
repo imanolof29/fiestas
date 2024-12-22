53f7418f93e09804a3ec55f0ee57d4f960be0d2f
imanolortizfernandez <imanolof29@gmail.com>
2024-12-22 11:53:35 +0100

    feat: location permission

--- provider/LocationProvider.tsx
+++ provider/LocationProvider.tsx
@@ -1,5 +1,7 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 import * as Location from 'expo-location';
+import { LocationPermission } from "@/types/location-permission";
+import { Alert } from "react-native";
 
 interface LocationContextType {
     location: Location.LocationObject | null;
@@ -7,6 +9,9 @@ interface LocationContextType {
     radius: number
     setRadius: (value: number) => void
     getCurrentLocation: () => Promise<void>;
+    requestLocationPermission: () => Promise<LocationPermission>;
+    checkLocationPermission: () => Promise<LocationPermission>;
+    manualPermissionRequest: () => void;
 }
 
 const LocationContext = createContext<LocationContextType | undefined>(undefined);
@@ -15,6 +20,42 @@ export const LocationProvider = ({ children }: PropsWithChildren) => {
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
     const [radius, setRadius] = useState<number>(5000)
+
+    const requestLocationPermission = async (): Promise<LocationPermission> => {
+        const { status } = await Location.requestBackgroundPermissionsAsync();
+        if (status === 'granted') {
+            if (status === 'granted') {
+
+            }
+            return LocationPermission.DENIED
+        }
+        return LocationPermission.GRANTED
+    }
+
+    const checkLocationPermission = async (): Promise<LocationPermission> => {
+        const { status } = await Location.getBackgroundPermissionsAsync();
+        switch (status) {
+            case 'granted':
+                return LocationPermission.GRANTED
+            case 'denied':
+                return LocationPermission.DENIED
+            default:
+                return LocationPermission.UNDETERMINED
+        }
+    }
+
+    const manualPermissionRequest = async () => {
+        Alert.alert(
+            'Permisos de ubicación',
+            'Para poder utilizar la aplicación necesitamos acceder a tu ubicación',
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel'
+                },
+            ]
+        )
+    }
 
     const getCurrentLocation = async () => {
         try {
@@ -34,7 +75,7 @@ export const LocationProvider = ({ children }: PropsWithChildren) => {
     };
 
     return (
-        <LocationContext.Provider value={{ location, errorMsg, getCurrentLocation, radius, setRadius }}>
+        <LocationContext.Provider value={{ location, errorMsg, getCurrentLocation, radius, setRadius, checkLocationPermission, requestLocationPermission, manualPermissionRequest }}>
             {children}
         </LocationContext.Provider>
     );
---
+++ types/location-permission.ts
@@ -0,0 +1,9 @@
+export enum LocationPermission {
+    CHECKING = 'checking',
+    GRANTED = 'GRANTED',
+    DENIED = 'DENIED',
+    BLOCKED = 'BLOCKED',
+    LIMITED = 'LIMITED',
+    UNAVAILABLE = 'UNAVAILABLE',
+    UNDETERMINED = 'UNDETERMINED',
+}
